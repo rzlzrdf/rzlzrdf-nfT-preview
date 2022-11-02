b2d2c0ec76998c5f6e6b853bf11e6cfb1711b9f1
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-11-03 06:41:50 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container min-h-screen max-w-full bg-darkblue flex justify-center items-center">
+      <div className='h-fit w-64 bg-blueblur p-4 rounded-2xl shadow-2xl'>
+        <div>
+          <img className='relative w-64 rounded-xl mb-4 hover:bg-cyan-400 hover:cursor-pointer hover:opacity-0' src='./img/image-equilibrium.jpg' alt='' />
+          <div className='absolute top-28 rounded-xl mt-2 bg-cyan-500 w-56 h-56 z-50 opacity-0 hover:cursor-pointer hover:opacity-80 transition-opacity flex justify-center items-center' >
+            <img className='w-8' src='./img/icon-view.svg' alt='' />
+          </div>
+        </div>
+        <h1 className='text-white text-lg font-sans font-semibold mb-4 hover:text-cyan-400 hover:cursor-pointer'>Equilibrium #3429</h1>
+        <h2 className='text-gray-400 font-sans font-normal text-sm mb-3'>Our Equilibrium collection promotes balance and calm.</h2>
+        <div className='flex justify-between mb-3'>
+          <div>
+            <img className='inline w-2 h-3' src='./img/icon-ethereum.svg' alt='' /><span className='text-cyan-400 text-xs font-semibold'> 0.041ETH</span>
+          </div>
+          <div>
+            <img className='inline w-3' src='./img/icon-clock.svg' alt='' /><span className='text-gray-400 text-xs font-semibold'> 3 Days Left</span>
+          </div>
+        </div>
+        <div className='border-t border-gray-400 pt-3'>
+          <img className='w-7 inline border-solid border-2 border-gray-400 rounded-full' src='./img/image-avatar.png' alt='' />
+          <h2 className='inline font-sans font-semibold text-xs text-gray-400 ml-2'> Creation of<span className='text-white hover:text-cyan-400 hover:cursor-pointer'> Jules Wyvern</span></h2>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'darkblue': '#0D1A2D',
+        'blueblur': '#15273F',
+      },
+    },
+  },
+  plugins: [],
+}
